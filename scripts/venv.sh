43dded75fefddef639e5d97efb3ed1b1bd4fc19f
Everly <66821749+everlyy@users.noreply.github.com>
2025-01-30 12:33:11 +0100

    Patch up `scripts/venv.sh` to work with GitHub actions

--- scripts/venv.sh
+++ scripts/venv.sh
@@ -9,17 +9,17 @@ function activate {
     source $VENV_DIR/bin/activate
 }
 
-if [[ ! -f "$CONFIG_FILE" ]] then
+if [ ! -f "$CONFIG_FILE" ]; then
     # Create example config file
-    # NOTE: Empty for now
     echo "LASTFM_API_KEY = \"abcef0123456789\"" >> $CONFIG_FILE
     echo "LASTFM_API_SECRET = \"abcef0123456789\"" >> $CONFIG_FILE
 fi
 
-if [[ ! -d "$VENV_DIR" ]] then
+if [ ! -d "$VENV_DIR" ]; then
     # Create virtual environment and install required packages
     $PYTHON -m venv "$VENV_DIR"
     activate
+    pip install --upgrade pip
     pip install -r "$REQUIREMENTS"
 else
     activate
