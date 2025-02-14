c8d120ec607b09b4bceaf8340da336b48475eee4
Everly <66821749+everlyy@users.noreply.github.com>
2025-02-14 20:26:39 +0100

    Remove TODO

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -3,6 +3,4 @@
 set -eo pipefail
 
 source scripts/venv.sh
-# TODO: I want this to be `mypy .` but there's some shenanigans about relative
-#       imports which I can't be bothered to fix at the moment
 mypy passenger_wsgi.py
